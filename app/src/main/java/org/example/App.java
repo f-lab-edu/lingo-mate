7cb47611ac4c7a0ed939272be5289aa0bada00dc
gdrffg <gdrffg@gmail.com>
2024-12-04 21:30:45 +0900

    update: 12/4

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -6,7 +6,6 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.swing.*;
 
 @SpringBootApplication
 public class App {
