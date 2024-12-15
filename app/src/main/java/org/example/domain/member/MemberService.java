4ddcc8846c27878d6e7173002ff3f6eb8bd35f64
gdrffg <gdrffg@gmail.com>
2024-12-15 18:36:32 +0900

    test: MemberControllerIntegrationTest
    1. TestRestTemplate를 이용하여
    MemberControllerIntegrationTest를 작성했습니다.
    
    2. MemberService에서는 더 이상 사용하지 않은 import문을 삭제했습니다.

--- app/src/main/java/org/example/domain/member/MemberService.java
+++ app/src/main/java/org/example/domain/member/MemberService.java
@@ -1,14 +1,11 @@
 package org.example.domain.member;
 
-import jakarta.validation.Valid;
-import org.example.domain.common.ApiResponse;
 import org.example.domain.member.dto.MemberEditForm;
 import org.example.domain.member.dto.MemberForm;
 import org.example.domain.member.entity.Member;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Service
 public class MemberService {
@@ -35,7 +32,11 @@ public class MemberService {
     @PutMapping("/{user_id}/edit")
     public Member updateMember(Long user_id, MemberEditForm memberEditForm){
         Member member = memberRepository.findById(user_id);
-        member.editMember(memberEditForm);
-        return member;
+
+        if(member != null) {
+            return member.editMember(memberEditForm);
+        } else {
+            return null;
+        }
     }
 }
